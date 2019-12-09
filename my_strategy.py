08ee345f99ef53be623e48031ebc3848c2895399
sashkavolkov <sashka.volkov@gmail.com>
2019-12-09 23:30:10 +0100

    velocity fix v.2.0.1

--- my_strategy.py
+++ my_strategy.py
@@ -1,5 +1,5 @@
 import model
-
+import math
 
 class MyStrategy:
     def __init__(self):
@@ -19,13 +19,13 @@ class MyStrategy:
                 box.item, model.Item.Weapon), game.loot_boxes),
             key=lambda box: distance_sqr(box.position, unit.position),
             default=None)
-        #nearest Riffle
+        # nearest Riffle
         nearest_riffle = min(
         filter(lambda box: isinstance(
             box.item, model.Item.Weapon) and box.item.weapon_type == model.WeaponType.ASSAULT_RIFLE, game.loot_boxes),
         key=lambda box: distance_sqr(box.position, unit.position),
         default=None)
-        #nearest healthpack
+        # nearest healthpack
         nearest_healthpack = min(
             filter(lambda box: isinstance(
                 box.item, model.Item.HealthPack), game.loot_boxes),
@@ -45,7 +45,7 @@ class MyStrategy:
              weapon_type = ''
         shoot = True
         swap_weapon = False
-        #Weapon
+        # Weapon
         if unit.weapon is None and nearest_weapon is not None:
             target_pos = nearest_weapon.position
         elif unit.weapon is not None and weapon_type == model.WeaponType.ROCKET_LAUNCHER:
@@ -62,15 +62,15 @@ class MyStrategy:
             target_pos = nearest_healthpack.position
         elif nearest_enemy is not None:
             target_pos = nearest_enemy.position
-        #debug
+        # debug
         debug.draw(model.CustomData.Log("Target pos: {}".format(target_pos)))
-        #Aim
+        # Aim
         aim = model.Vec2Double(0, 0)
         if nearest_enemy is not None:
             aim = model.Vec2Double(
                 nearest_enemy.position.x - unit.position.x,
                 nearest_enemy.position.y - unit.position.y)
-        #Jump
+        # Jump
         jump = target_pos.y > unit.position.y
         if target_pos.x > unit.position.x and game.level.tiles[int(unit.position.x + 1)][int(unit.position.y)] == model.Tile.WALL:
             jump = True
@@ -78,16 +78,15 @@ class MyStrategy:
             jump = True
         # if target_pos == nearest_enemy.position:
         #     jump = True
-        #velocity
+        # velocity
         velocity = target_pos.x - unit.position.x
-        # print(velocity)
         if velocity < 0 and velocity > -10:
             velocity = -10
         elif velocity > 0 and velocity < 10:
             velocity = 10
         if target_pos == nearest_enemy.position and abs(target_pos.x - unit.position.x)<2:
-            velocity = 0.2
-        # print(velocity)
+            # velocity = 0.2
+            velocity = math.copysign(0.2, target_pos.x - unit.position.x)
         return model.UnitAction(
             velocity=velocity,
             jump=jump,
